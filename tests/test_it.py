98658e1df7ea73c7c7bfb665929644c1fc207edd
Michael Merickel <michael@merickel.org>
2016-11-27 14:04:31 -0600

    depend on new pytest-venv package

--- tests/test_it.py
+++ tests/test_it.py
@@ -1,38 +1,7 @@
-import os
-import pytest
 import sys
 import subprocess
 
 WIN = sys.platform == 'win32'
-
-@pytest.fixture
-def venv(tmpdir):
-    venv = VirtualEnvironment(tmpdir.strpath)
-    venv.create()
-    yield venv
-
-
-class VirtualEnvironment(object):
-    def __init__(self, path):
-        self.path = path
-        self.bin = os.path.join(
-            self.path,
-            'bin' if sys.platform != 'win32' else 'Scripts',
-        )
-        self.python = os.path.join(self.bin, 'python')
-
-    def create(self):
-        subprocess.check_call(
-            [sys.executable, '-m', 'virtualenv', self.path],
-        )
-
-    def install(self, pkg_name, editable=False, cwd=None):
-        cmd = [self.python, '-m', 'pip', 'install']
-        if editable:
-            cmd += ['-e']
-        cmd += [pkg_name]
-        subprocess.check_call(cmd, cwd=cwd)
-
 
 def test_it(cookies, venv, capfd):
     result = cookies.bake(extra_context={
