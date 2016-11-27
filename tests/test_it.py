7a995e0f284d080bf315a8df5b216fbc3b967181
Michael Merickel <michael@merickel.org>
2016-11-27 12:57:31 -0600

    validate the module name

--- tests/test_it.py
+++ tests/test_it.py
@@ -5,15 +5,6 @@ import subprocess
 
 WIN = sys.platform == 'win32'
 
-
-@pytest.fixture
-def context():
-    return {
-        'project_name': 'Test Project',
-        'repo_name': 'myapp',
-    }
-
-
 @pytest.fixture
 def venv(tmpdir):
     venv = VirtualEnvironment(tmpdir.strpath)
@@ -43,8 +34,11 @@ class VirtualEnvironment(object):
         subprocess.check_call(cmd, cwd=cwd)
 
 
-def test_it(cookies, context, venv, capfd):
-    result = cookies.bake(extra_context=context)
+def test_it(cookies, venv, capfd):
+    result = cookies.bake(extra_context={
+        'project_name': 'Test Project',
+        'repo_name': 'myapp',
+    })
     assert result.exit_code == 0
     assert result.project.basename == 'myapp'
 
@@ -57,3 +51,10 @@ def test_it(cookies, context, venv, capfd):
     cwd = result.project.strpath
     venv.install('.[testing]', editable=True, cwd=cwd)
     subprocess.check_call([venv.python, '-m', 'pytest', '-q'], cwd=cwd)
+
+
+def test_it_invalid_module_name(cookies, venv, capfd):
+    result = cookies.bake(extra_context={
+        'repo_name': '0invalid',
+    })
+    assert result.exit_code == -1
