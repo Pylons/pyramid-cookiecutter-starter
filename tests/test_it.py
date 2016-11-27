cb257b57c0bbe84a5cfdc7e81763acd39334aa4b
Michael Merickel <michael@merickel.org>
2016-11-27 16:38:26 -0600

    run tests against pyramid master

--- tests/test_it.py
+++ tests/test_it.py
@@ -1,9 +1,14 @@
+import pytest
 import sys
 import subprocess
 
 WIN = sys.platform == 'win32'
 
-def test_it(cookies, venv, capfd):
+@pytest.mark.parametrize('pyramid_dependency', [
+    ('pyramid', False),
+    ('git+https://github.com/Pylons/pyramid.git@master#egg=pyramid', True),
+])
+def test_it(pyramid_dependency, cookies, venv, capfd):
     result = cookies.bake(extra_context={
         'project_name': 'Test Project',
         'repo_name': 'myapp',
@@ -18,6 +23,7 @@ def test_it(cookies, venv, capfd):
         assert 'bin/pserve' in out
 
     cwd = result.project.strpath
+    venv.install(pyramid_dependency[0], editable=pyramid_dependency[1])
     venv.install(cwd + '[testing]', editable=True)
     subprocess.check_call([venv.python, '-m', 'pytest', '-q'], cwd=cwd)
 
