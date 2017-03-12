5bc8aa02747f83f65392585f022fed5c49dedaf8
Petr Blahos <petrblahos@gmail.com>
2017-03-12 21:21:36 +0100

    Adds support for mako templates
    Instead of removing lines for unused templates we use the
    template extension to generate the right template filename
    for view_config, as well as for including the right template
    module (pyramid_(mako|jinja2|chameleon)).

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -15,53 +15,20 @@ def clean_unused_template_settings():
     working = os.path.abspath(os.path.join(os.path.curdir))
     templates = os.path.join(working, '{{cookiecutter.repo_name}}', 'templates')
 
-    layout = None
-    mytemplate = None
-    removal_text = None
-    view = os.path.join(working, '{{cookiecutter.repo_name}}', 'views.py')
-
-    if selected_lang == 'jinja2':
-        removal_text = ['chameleon', 'mytemplate.pt']
-        layout = os.path.join(templates, 'layout.pt')
-        mytemplate = os.path.join(templates, 'mytemplate.pt')
-    elif selected_lang == 'chameleon':
-        removal_text = ['jinja2']
-        layout = os.path.join(templates, 'layout.jinja2')
-        mytemplate = os.path.join(templates, 'mytemplate.jinja2')
-
-    delete_files([layout, mytemplate])
-
-    setup = os.path.join(working, 'setup.py')
-    init = os.path.join(working, '{{cookiecutter.repo_name}}', '__init__.py')
-    remove_lines_from_files([setup, init, view], removal_text)
+    if selected_lang == "chameleon":
+        extension = ".pt"
+    else:
+        extension = "." + selected_lang
+    delete_other_ext(templates, extension)
 
 
-def delete_files(files):
-    for file in files:
-        if not os.path.exists(file):
-            print("WARNING: Cookiecutter post generation cannot find file: " + file)
-        else:
-            os.remove(file)
-
-
-def remove_lines_from_files(files, substring_entries):
-    for file in files:
-        if not os.path.exists(file):
-            print("WARNING: Post generation cannot find file: " + file)
-            continue
-
-        with open(file, mode='r') as fin:
-            lines = fin.readlines()
-
-        with open(file, mode='w') as fout:
-            for line in lines:
-                skip_line = False
-                for substring in substring_entries:
-                    if line.find(substring) >= 0:
-                        skip_line = True
-
-                if not skip_line:
-                    fout.write(line)
+def delete_other_ext(directory, extension):
+    """
+    Removes all files not ending with the extension.
+    """
+    for i in os.listdir(directory):
+        if not i.endswith(extension):
+            os.unlink(os.path.join(directory, i))
 
 
 def display_actions_message():
@@ -90,22 +57,22 @@ def display_actions_message():
         Mailing List:  https://groups.google.com/forum/#!forum/pylons-discuss
         Welcome to Pyramid.  Sorry for the convenience.
         %(separator)s
-    
+
         Change directory into your newly created project.
             cd {{ cookiecutter.repo_name }}
-    
+
         Create a Python virtual environment.
             %(venv_cmd)s %(venv)s
-    
+
         Upgrade packaging tools.
             %(pip_cmd)s install --upgrade pip setuptools
-    
+
         Install the project in editable mode with its testing requirements.
             %(pip_cmd)s install -e ".[testing]"
-    
+
         Run your project's tests.
             %(pytest_cmd)s
-    
+
         Run your project.
             %(pserve_cmd)s development.ini
         """ % vars)
