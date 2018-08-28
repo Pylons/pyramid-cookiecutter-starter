d3d7a5842616a1ee6dd025b63c7e6f69f510a722
Steve Piercy <web@stevepiercy.com>
2018-08-28 00:08:11 -0700

    Use a context manager to configure the app - Closes #44

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/__init__.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/__init__.py
@@ -4,8 +4,8 @@ from pyramid.config import Configurator
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
-    config = Configurator(settings=settings)
-    config.include('pyramid_{{ cookiecutter.template_language }}')
-    config.include('.routes')
-    config.scan()
+    with Configurator(settings=settings) as config:
+        config.include('pyramid_{{ cookiecutter.template_language }}')
+        config.include('.routes')
+        config.scan()
     return config.make_wsgi_app()
