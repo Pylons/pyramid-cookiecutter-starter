d3e14c134d38169a03bf5a14f4cd3f707fa696a6
Michael Merickel <michael@merickel.org>
2019-12-29 22:35:22 -0600

    move tests outside

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -3,7 +3,7 @@ import sys
 import shutil
 from textwrap import dedent
 
-WORKING = os.path.abspath(os.path.join(os.path.curdir))
+WORKING = os.path.abspath(os.path.curdir)
 
 
 def main():
@@ -46,10 +46,7 @@ def clean_unused_backend():
         prefix = 'zodb_'
         rm_prefixes = ['sqlalchemy_']
 
-    scaffold_directory = os.path.join(
-                WORKING, '{{cookiecutter.repo_name}}')
-
-    delete_other_files(scaffold_directory, prefix, rm_prefixes)
+    delete_other_files(WORKING, prefix, rm_prefixes)
 
 
 def delete_other_files(directory, current_prefix, rm_prefixes):
@@ -65,7 +62,9 @@ def delete_other_files(directory, current_prefix, rm_prefixes):
         base_prefix = 'base_'
         if filename.startswith(base_prefix):
             filename = filename[len(base_prefix):]
-            os.rename(full_path, os.path.join(directory, filename))
+            to_path = os.path.join(directory, filename)
+            os.rename(full_path, to_path)
+            full_path = to_path
 
         for rm_prefix in rm_prefixes:
 
@@ -79,6 +78,9 @@ def delete_other_files(directory, current_prefix, rm_prefixes):
             elif current_prefix and filename.startswith(current_prefix):
                 filename = filename[len(current_prefix):]
                 os.rename(full_path, os.path.join(directory, filename))
+
+        if os.path.isdir(full_path):
+            delete_other_files(full_path, current_prefix, rm_prefixes)
 
 
 def display_actions_message():
