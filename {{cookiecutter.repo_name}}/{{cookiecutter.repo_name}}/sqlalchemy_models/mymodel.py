2f21ecd926f282214a44ee5d3b1a5c626f7de08a
Michael Merickel <michael@merickel.org>
2024-02-04 11:10:21 -0700

    update mapper patterns following sqla orm migration guide

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/sqlalchemy_models/mymodel.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/sqlalchemy_models/mymodel.py
@@ -1,18 +1,15 @@
-from sqlalchemy import (
-    Column,
-    Index,
-    Integer,
-    Text,
-)
+from sqlalchemy import Index, Integer, Text
+from sqlalchemy.orm import Mapped, mapped_column
+from typing import Optional
 
 from .meta import Base
 
 
 class MyModel(Base):
     __tablename__ = 'models'
-    id = Column(Integer, primary_key=True)
-    name = Column(Text)
-    value = Column(Integer)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[Optional[str]] = mapped_column(Text)
+    value: Mapped[Optional[int]] = mapped_column(Integer)
 
 
 Index('my_index', MyModel.name, unique=True, mysql_length=255)
